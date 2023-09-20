2bc30f20e7ed55f6ce986138f4432395616168f0
Gustavo Rodrigues <108638526+GustavoRRSilva@users.noreply.github.com>
2023-09-20 10:16:47 -0300

    Criação do primeiro componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
+import FirstComponent from '../componentes/FirstComponent/index.jsx';
 import './App.css';
 function App() {
   return (
     <div>
       <p>Hello world </p>
       <p>Troca pelo vscode online</p>
+      <FirstComponent/>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/componentes/FirstComponent/index.jsx
@@ -0,0 +1,9 @@
+ import React from 'react'
+ import "./FirstComponent.css"
+ function FirstComponent() {
+   return (
+     <div>FirstComponent</div>
+   )
+ }
+ 
+ export default FirstComponent
