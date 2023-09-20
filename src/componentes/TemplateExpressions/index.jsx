c37a06ec0a0a941c33dadca80296f8c439d80f89
GustavoRRSilva <108638526+GustavoRRSilva@users.noreply.github.com>
2023-09-20 12:09:52 -0300

    Fim da aula template Expressions

--- src/componentes/TemplateExpressions/index.jsx
+++ src/componentes/TemplateExpressions/index.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 function TemplateExpressions() {
   const name = "Gustavo";
+  const data = {
+    name:"Gustavo",
+    job:"Front-End DEV"
+  }
   return (
-    <div>Olá {name}!</div>
+    <div>
+      <h1>Olá {name}</h1>
+      <h1>Seu trabalho é:{data.job}</h1>
+    </div>
   )
 }
 
