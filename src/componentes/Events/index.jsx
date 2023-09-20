827e3ba574a709d74761aa347380f126462c8aee
GustavoRRSilva <108638526+GustavoRRSilva@users.noreply.github.com>
2023-09-20 12:42:05 -0300

    Fim da aula Função de renderização

--- src/componentes/Events/index.jsx
+++ src/componentes/Events/index.jsx
@@ -6,11 +6,25 @@ const Events = ()=>{
     alert(e)
     alert("evento acionado")
   }
+  const handleSomething = (x)=>{
+    if(x){
+      return <h1>renderizando isso</h1>;
+    }
+    else{
+      return <h1> não renderizado </h1>;
+    }
+  }
   return(
     <div>
         <div>
           <button onClick = {handleMyEvent}>Clique aqui</button>
         </div>
+        <div>
+          <button onClick = {() => {
+            alert("fui clicado tbm")
+          }}>Clique aqui também</button>
+        </div>
+        {handleSomething(false)}
     </div>
   )
 }
