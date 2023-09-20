b18ffa7647eba3a3cc2bd969f2941d5f9dc0b1a4
GustavoRRSilva <108638526+GustavoRRSilva@users.noreply.github.com>
2023-09-20 11:56:16 -0300

    meio da aula Template Expressions

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,17 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import FirstComponent from './componentes/FirstComponent/index.jsx';
+import TemplateExpressions from './componentes/TemplateExpressions/index.jsx';
 import './App.css';
 function App() {
   return (
     <div>
       <p>Hello world </p>
       <p>Troca pelo vscode online</p>
-      <FirstComponent/>
+      <FirstComponent />
+      <TemplateExpressions />
     </div>
   );
 }
 
 export default App;
-
---
+++ src/componentes/TemplateExpressions/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function TemplateExpressions() {
+  const name = "Gustavo";
+  return (
+    <div>Olá {name}!</div>
+  )
+}
+
+export default TemplateExpressions;
