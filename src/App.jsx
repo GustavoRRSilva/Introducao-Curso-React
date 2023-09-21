8249076c560b22b14ce3134cf2572d1a0d4124c4
Gustavo <gstvrrds@gmail.com>
2023-09-20 23:23:56 -0300

    começando o terceiro capitulo

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,12 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 //Componentes
-import FirstComponent from './componentes/FirstComponent/index.jsx';
-import TemplateExpressions from './componentes/TemplateExpressions/index.jsx';
-import Events from './componentes/Events/index.jsx';
 //Css
 import './App.css';
 function App() {
   return (
     <div className  = "app">
-      <p>Hello world </p>
-      <p>Troca pelo vscode online</p>
-      <FirstComponent />
-      <TemplateExpressions />
-      <Events />
+      <h1>Começo da seção 3</h1>
     </div>
   );
 }
