527dd648055d137cf86d6dbba912516efde056ea
GustavoRRSilva <108638526+GustavoRRSilva@users.noreply.github.com>
2023-09-21 11:12:02 -0300

    Fim da aula Imagens em Src

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 //Libs react
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 //Componentes
 //Css
+//Imagens 
+import ImagemReact from './assets/react.svg'
 import './App.css';
 function App() {
   return (
     <div className  = "app">
       <h1>Começo da seção 3</h1>
+      <img src="./public/vite.svg" alt="Icon vite"/>
+      <img src={ImagemReact} alt="Imagem React"/>
     </div>
   );
 }
